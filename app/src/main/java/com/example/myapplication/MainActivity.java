23c7cc50228d96cf5a7e6b07826a5228519c6e29
Naramazuki <mathisreine@outlook.com>
2022-11-21 20:59:28 +0100

    Ajout écran d'acceuil avec page de connexion

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button Lancer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lancer =(Button)findViewById(R.id.button);
+        Lancer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                Intent otherActivity = new Intent (MainActivity.this, Page_Menu.class);
+                startActivity(otherActivity);
+
+            }
+        });
     }
+    public void go(View view){
+
+        TextView output= findViewById(R.id.Affichage1);
+        output.setText("t'as appuyé sur le bouton");
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/Page_Menu.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_menu);
+    }
+}
