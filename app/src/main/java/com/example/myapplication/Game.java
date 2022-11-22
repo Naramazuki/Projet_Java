8eb692ecffb9d9a7872a6950f07a396daf318b19
Mathis REINE <103426705+Naramazuki@users.noreply.github.com>
2022-11-22 21:28:45 +0100

    ajout de la selection de pseudo

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Game extends AppCompatActivity {
 
@@ -12,5 +17,16 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Button Entrer = findViewById(R.id.Entrer);
+        TextView Pseudo = findViewById(R.id.Pseudo);
+        ConstraintLayout layout=findViewById(R.id.Background);
+
+        Entrer.setOnClickListener(view -> {
+            layout.setBackgroundResource(R.drawable.battle_background);
+            Entrer.setVisibility(View.INVISIBLE);
+            Pseudo.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Bienvenue "+ Pseudo.getText().toString(),Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
