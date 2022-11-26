4e07b68c407cc5009443be7ca5e67d845e560207
Mathis REINE <103426705+Naramazuki@users.noreply.github.com>
2022-11-26 11:45:31 +0100

    Fonction bouton next

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,13 +1,13 @@
 package com.example.myapplication;
 
 import android.content.pm.ActivityInfo;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
@@ -18,49 +18,91 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Game extends AppCompatActivity {
+    public void get_character(int id,TextView mana, TextView Pv,ListCharacters listp,TextView Nom){
+
+        mana.setText(listp.getTeam().get(id).mana_act+"/"+listp.getTeam().get(id).mana+"PM");
+        Pv.setText(listp.getTeam().get(id).hp_act+"/"+listp.getTeam().get(id).hp+"PV");
+        Nom.setText(listp.getTeam().get(id).name);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        final int[] i = {0};
         setContentView(R.layout.activity_game);
         View overlay=findViewById(R.id.Background);
         overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         Button Entrer = findViewById(R.id.Entrer);
-        TextView Pseudo = findViewById(R.id.Pseudo);
+        EditText Pseudo = findViewById(R.id.Pseudo);
         TextView mana = findViewById(R.id.Mana);
         TextView Pv = findViewById(R.id.Pv);
-
+        Button Next=findViewById(R.id.NextChara);
         ConstraintLayout layout=findViewById(R.id.Background);
         TableLayout interfaceCombat= findViewById(R.id.IntfCombat);
         ProgressBar bar =findViewById(R.id.progressBar3);
         TableLayout interfacePersonnage=findViewById(R.id.personnage);
         TextView Nom=findViewById(R.id.Nom);
         ListCharacters listp= new ListCharacters();
-        listp.addTeam(new Characters(Pseudo.getText().toString()));
         listp.addTeam(new Characters("deji"));
 
         ImageView imagePerso=findViewById(R.id.ImagePerso);
+        Pseudo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Entrer.setEnabled(!Pseudo.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+
+            }
+        });
 
 
         Entrer.setOnClickListener(view -> {
+            listp.addTeam(new Characters(Pseudo.getText().toString()));
+
             layout.setBackgroundResource(R.drawable.battle_background);
             Entrer.setVisibility(View.INVISIBLE);
             Pseudo.setVisibility(View.INVISIBLE);
             interfaceCombat.setVisibility(View.VISIBLE);
-            mana.setText(listp.getTeam().get(0).mana_act+"/"+listp.getTeam().get(0).mana+"PM");
-            Pv.setText(listp.getTeam().get(0).hp_act+"/"+listp.getTeam().get(0).hp+"PV");
+            get_character(0,mana,Pv,listp,Nom);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 bar.setProgress(100,true);
             }
+
             Toast.makeText(getApplicationContext(),"Bienvenue "+ Pseudo.getText().toString(),Toast.LENGTH_SHORT).show();
             bar.setVisibility(View.INVISIBLE);
-            Nom.setText(Pseudo.getText().toString());
+
             interfacePersonnage.setVisibility(View.VISIBLE);
             imagePerso.setVisibility(View.VISIBLE);
 
+        });
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i[0]<listp.getTeam().size()){
+                    get_character(i[0],mana,Pv,listp,Nom);
+                    i[0]++;
+
+                }
+
+                else {
+                    i[0]=0;
+                    get_character(i[0],mana,Pv,listp,Nom);
+                }
+
+
+
+            }
         });
 
     }
